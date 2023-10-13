dd53ac115982ecddb45d93f04c2444d758a90f30
Jeremy Kung <jeremykung@gmail.com>
2023-10-14 00:58:50 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Title</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, aliquam mollitia? Cum officia nostrum, quibusdam cupiditate velit minus. Veritatis ipsa illum voluptates hic est adipisci dignissimos corrupti iure? Quos, fugiat!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 2rem;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
